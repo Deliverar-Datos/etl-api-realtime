b973b62dba6c24eea882be71f82dc4bdfbe8a828
matiasbiagetti <biagettimati@gmail.com>
2025-06-26 00:32:09 -0300

    cors

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from app.events.routers.callback import router as callback_router
 from app.events.routers.role import router as role_router
 from app.models.database import Base, engine
 from app.core.config import settings
+from fastapi.middleware.cors import CORSMiddleware
 
 # Create database tables
 Base.metadata.create_all(bind=engine)
@@ -19,6 +20,14 @@ app = FastAPI(
     description="Handles events from different guilds and transforms to star schema",
     version=settings.APP_VERSION,
     debug=settings.DEBUG
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Permite cualquier origen
+    allow_credentials=True,
+    allow_methods=["*"],  # Permite todos los métodos HTTP
+    allow_headers=["*"],  # Permite todos los headers
 )
 
 app.include_router(callback_router, prefix="/events")
